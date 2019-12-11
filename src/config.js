8e7d5a39aaf1a1fa0cdabb7aa11a9184f6278592
Charlie <charlesbadjr@gmail.com>
2019-12-10 17:57:57 -0700

    one way of doing it

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+export const clientId = "47342e043f2342deaf0ce8d096ea7205";
+export const redirectUri = "http//localhost:3000/";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
